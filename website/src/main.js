70fa1701053ae4b08a2e2f4a6ff6aa8871e7264b
Snir Shechter <snir@aporia.com>
2021-04-21 19:45:39 +0300

    Initial commit

--- website/src/main.js
+++ website/src/main.js
@@ -3,7 +3,7 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default async function(Vue, { isClient }) {
 	if (isClient) {
 		const { firebaseMessagingService } = await import('./services/firebase/messaging.js')
-		firebaseMessagingService.start(navigator.serviceWorker.register('/sw.js'))
+		firebaseMessagingService.start(await navigator.serviceWorker.register('/sw.js'))
 	}
 	Vue.component('Layout', DefaultLayout)
 }
