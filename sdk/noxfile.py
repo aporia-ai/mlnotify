706212cc963fb307d591550251752c75ea32e8f0
Snir Shechter <snir@aporia.com>
2021-08-10 18:05:24 +0300

    Tests

--- sdk/noxfile.py
+++ sdk/noxfile.py
@@ -47,7 +47,7 @@ def install_with_constraints(session: Session, *args: str, **kwargs: Any):
 @nox.session(python=["3.8", "3.7", "3.6"])
 def tests(session: Session) -> None:
     """Run the test suite."""
-    tensorflow = "tensorflow==2.1.4" if ((session.python == "3.6") or (session.python == "3.7")) else "tensorflow"
+    tensorflow = "tensorflow==2.1.4" if (session.python == "3.6") else "tensorflow"
     print(session.python + " " + tensorflow)
     install_with_constraints(session, "pytest", tensorflow, "xgboost", "lightgbm", "scikit-learn")
     session.install(".", "--upgrade")
