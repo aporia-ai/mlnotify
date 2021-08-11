595a831773cd092bbc69bba85524f995122e94ed
Snir Shechter <snir@aporia.com>
2021-08-11 14:00:29 +0300

    Removes python 3.9

--- sdk/noxfile.py
+++ sdk/noxfile.py
@@ -44,11 +44,9 @@ def install_with_constraints(session: Session, *args: str, **kwargs: Any):
         shutil.rmtree(dirname)
 
 
-@nox.session(python=["3.9", "3.8", "3.7", "3.6"])
+@nox.session(python=["3.8", "3.7"])
 def tests(session: Session) -> None:
     """Run the test suite."""
-    tensorflow = "tensorflow==2.1.4" if (session.python == "3.6") else "tensorflow"
-    print(session.python + " " + tensorflow)
-    install_with_constraints(session, "pytest", tensorflow, "xgboost", "lightgbm", "scikit-learn")
+    install_with_constraints(session, "pytest", "tensorflow", "xgboost", "lightgbm", "scikit-learn")
     session.install(".", "--upgrade")
     session.run("pytest", *session.posargs)
