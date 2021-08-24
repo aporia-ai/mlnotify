37603ef8e7b97349d923e1239aa5532e034264f7
Snir Shechter <snir@aporia.com>
2021-08-24 18:25:19 +0300

    Adds SEO

--- website/src/main.ts
+++ website/src/main.ts
@@ -13,6 +13,37 @@ const client: ClientApiConstructor = async function(
 ): Promise<void> {
 	head.htmlAttrs = { lang: 'en-US' }
 
+	// Social meta tags
+	const metaObjects = [
+		{ name: 'og:type', content: 'website' },
+		{ name: 'og:url', content: 'https://mlnotify.aporia.com/' },
+		{ name: 'og:title', content: 'ML Notify' },
+		{
+			name: 'og:description',
+			content:
+				'MLNotify is an open-source tool that keeps track of model training for you, and sends web, mobile, or email  notifications the second training is complete.',
+		},
+		{
+			name: 'og:image',
+			content: `${process.env.GRIDSOME_BASE_URL}/seo/image.png`,
+		},
+		{ name: 'twitter:card', content: 'summary_large_image' },
+		{ name: 'twitter:url', content: 'https://mlnotify.aporia.com/' },
+		{ name: 'twitter:title', content: 'ML Notify' },
+		{
+			name: 'twitter:description',
+			content:
+				'MLNotify is an open-source tool that keeps track of model training for you, and sends web, mobile, or email  notifications the second training is complete.',
+		},
+		{
+			name: 'twitter:image',
+			content: `${process.env.GRIDSOME_BASE_URL}/seo/image.png`,
+		},
+	]
+
+	if (!head.meta) head.meta = []
+	head.meta.push(...metaObjects)
+
 	// Toasts
 	if (isClient) {
 		import('vue-toast-notification').then(({ default: VueToast }) => {
