bed610ecbd4ef46009541ff09cf99ba8554b39d4
Alon Gubkin <alon.gubkin@gmail.com>
2021-08-25 19:01:34 +0300

    [v1.0.42] Improve website snippets (#37)

--- website/src/components/UsageInstructionsSection.vue
+++ website/src/components/UsageInstructionsSection.vue
@@ -1,10 +1,12 @@
 <template>
 	<section>
 		<div class="font-orelega md:text-lg text-xs md:mb-4 mb-3">1. Install package</div>
-		<CodeBlock>pip3 install mlnotify</CodeBlock>
+		<CodeBlock class="md:mb-10 mb-6"><pre>pip3 install mlnotify</pre></CodeBlock>
 		<div class="font-orelega md:text-lg text-xs md:mb-4 mb-3">2. Import package to code</div>
-		<CodeBlock>import mlnotify</CodeBlock>
-		<div class="font-orelega md:text-lg text-xs">3. Start training and copy the training code</div>
+		<CodeBlock class="md:mb-10 mb-6"><pre>import mlnotify</pre></CodeBlock>
+		<div class="font-orelega md:text-lg text-xs md:mb-4 mb-3">3. Run your model's training</div>
+		<CodeBlock class="md:mb-5 mb-3"><pre>model.fit(X_train, y_train, ...)</pre></CodeBlock>
+		<div class="text-xs ">...or just run <pre class="text-blue-1 inline">mlnotify.start()</pre> to test it quickly :)</div>
 	</section>
 </template>
 <script lang="ts">
